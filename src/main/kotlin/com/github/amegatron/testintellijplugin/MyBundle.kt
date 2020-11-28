04bbaf7d1ef3f8cd9d967c9bd308cf2fb0f10336
GitHub Action <action@github.com>
2020-11-28 11:25:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/amegatron/testintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/amegatron/testintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.amegatron.testintellijplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/amegatron/testintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.amegatron.testintellijplugin.services
+
+import com.github.amegatron.testintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
